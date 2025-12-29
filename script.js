53b328c81ac6288e5abf6661aba196b6c0fb1e7d
Ria Sommerfeld <sommerfeldria15@gmail.com>
2025-12-29 23:28:49 +0100

    enhance JavaScript task management functionality; improve styles for better user experience

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const uncompletedCounter = document.getElementById("uncompleted-counter");
 
 function updateCounters() {
   const completedTasks = document.querySelectorAll(".completed").length;
-  const uncompletedTasks =
-    document.querySelectorAll("li:not(.completed)").length;
+  const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
 
   completedCounter.textContent = completedTasks;
   uncompletedCounter.textContent = uncompletedTasks;
@@ -16,38 +15,56 @@ function addTask() {
   const task = inputBox.value.trim();
   if (!task) {
     alert("Please write down a task");
+    console.log("no task added");
+
     return;
   }
+
   const li = document.createElement("li");
   li.innerHTML = `
-  <label>
-    <input type="checkbox">
-    <span>${task}</span>
-  </label>
-  <span class="edit-btn">Edit</span>
-  <span class="delete-btn">Delete</span>
-`;
+    <label>
+      <input type="checkbox">
+      <span>${task}</span>
+    </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
   listContainer.appendChild(li);
-  inputBox.value = "";
+
+  inputBox.value = " ";
+
+  const checkbox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const taskSpan = li.querySelector("span");
+  const deleteBtn = li.querySelector(".delete-btn");
+
+  checkbox.addEventListener("click", function () {
+    li.classList.toggle("completed", checkbox.checked);
+    updateCounters();
+  });
+
+  editBtn.addEventListener("click", function () {
+    const update = prompt("Edit task:", taskSpan.textContent);
+    if (update !== null) {
+      taskSpan.textContent = update;
+      li.classList.remove("completed");
+      checkbox.checked = false;
+      updateCounters();
+    }
+  });
+
+  deleteBtn.addEventListener("click", function () {
+    if (confirm("Are you sure you want to delete this task?")) {
+      li.remove();
+      updateCounters();
+    }
+  });
   updateCounters();
 }
 
-const checkbox = li.querySelector("input");
-const editBtn = li.querySelector(".edit-btn");
-const taskSpan = li.querySelector("span");
-const deleteBtn = li.querySelector(".delete-btn");
-
-checkbox.addEventListener("click", function () {
-  li.classList.toggle("completed", checkbox.checked);
-  updateCounters();
-});
-
-editBtn.addEventListener("click", function () {
-  const update = prompt("Edit task:", taskSpan.textContent);
-  if (update !== null) {
-    taskSpan.textContent = update;
-    li.classList.remove("completed");
+inputBox.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    addTask();
   }
-  checkbox.checked = false;
-  updateCounters();
 });
